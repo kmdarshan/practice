7c1e6de7d63e1d9e0a254131dd7ab23286cc7991
darshan <darshan@darshans-macbook-pro-2.local>
2018-11-05 22:59:04 -0800

    New practice

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,45 @@
+//: Playground - noun: a place where people can play
+
 import UIKit
 
-var str = "Hello, playground"
+func reverseWordInPlace(_ str : inout String) -> Array<Character> {
+    var characters = Array(str)
+    var start = 0
+    var ending = characters.count - 1
+    while start < ending {
+        let char = characters[start]
+        characters[start] = characters[ending]
+        characters[ending] = char
+        start  += 1
+        ending -= 1
+    }
+    return characters
+}
+var word = "cake pound steal"
+//print(reverseWordInPlace(&word))
+
+func reverseWords(_ str : inout String) {
+    var reversedString = reverseWordInPlace(&str)
+    var fullWord = ""
+    var fullSentence = ""
+    for str in reversedString {
+        if(str != " ") {
+            fullWord = fullWord + String(str)
+        }
+        else {
+            if(fullSentence.count > 0) {
+                fullSentence = fullSentence + " " + reverseWordInPlace(&fullWord)
+            }
+            else {
+                fullSentence = String(reverseWordInPlace(&fullWord))
+            }
+            fullWord = ""
+        }
+    }
+    fullSentence = fullSentence + " " + reverseWordInPlace(&fullWord)
+    print(fullSentence)
+}
+
+var message = "cake pound steal"
+reverseWords(&message)
+print(message)
