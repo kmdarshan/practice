c57804326438941059e0b51404a9915e833dc020
darshan <darshan@darshans-macbook-pro-2.local>
2018-11-09 01:20:29 -0800

    BST implementation

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,6 +2,77 @@
 
 import UIKit
 
+class Node {
+    var value : Int
+    var lnode : Node? = nil
+    var rnode : Node? = nil
+    
+    init(_ value: Int) {
+        self.value = value
+    }
+    
+}
+
+class BST {
+    func insert(_ node : Node?, value: Int) -> Node? {
+        if node == nil {
+            return Node(value)
+        }
+        if(value < node!.value) {
+            node?.lnode = insert(node?.lnode, value: value)
+        } else if(value > node!.value) {
+            node?.rnode = insert(node?.rnode, value: value)
+        }
+        return node
+    }
+    
+    func inorderDisplay(_ node : Node?) {
+        if node != nil {
+            inorderDisplay(node?.lnode)
+            //print(node!.value)
+            inorderDisplay(node?.rnode)
+        }
+    }
+    
+    static var elements: [Int] = []
+    
+    func isTreeValid(_ node : Node?, values : [Int]) {
+        if node != nil {
+            isTreeValid(node?.lnode, values: BST.elements)
+            BST.elements.append(node!.value)
+            print(node!.value)
+            isTreeValid(node?.rnode, values: BST.elements)
+        }
+    }
+    
+    func isBSTValid() -> Bool {
+        bst.isTreeValid(node, values: BST.elements)
+        for i in BST.elements {
+            if(i+1 < BST.elements.count) {
+                if BST.elements[i] > BST.elements[i+1] {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
+
+var bst = BST()
+var node = bst.insert( nil, value: 50)
+node = bst.insert( node, value: 30)
+node = bst.insert( node, value: 20)
+node = bst.insert( node, value: 40)
+node = bst.insert( node, value: 70)
+node = bst.insert( node, value: 60)
+node = bst.insert( node, value: 80)
+//bst.inorderDisplay(node)
+if(bst.isBSTValid()) {
+    print("Valid BST")
+} else {
+    print("InValid BST")
+}
+
 func reverseWordInPlace(_ str : inout String) -> Array<Character> {
     var characters = Array(str)
     var start = 0
@@ -19,7 +90,7 @@ var word = "cake pound steal"
 //print(reverseWordInPlace(&word))
 
 func reverseWords(_ str : inout String) {
-    var reversedString = reverseWordInPlace(&str)
+    let reversedString = reverseWordInPlace(&str)
     var fullWord = ""
     var fullSentence = ""
     for str in reversedString {
@@ -37,9 +108,9 @@ func reverseWords(_ str : inout String) {
         }
     }
     fullSentence = fullSentence + " " + reverseWordInPlace(&fullWord)
-    print(fullSentence)
+    //print(fullSentence)
 }
 
 var message = "cake pound steal"
-reverseWords(&message)
-print(message)
+//reverseWords(&message)
+//print(message)
