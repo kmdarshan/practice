0df4eac46e1184cb487f326bee5ea6e343bfae7e
random ramblings <kmdarshan@gmail.com>
2019-10-29 06:49:38 -0700

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,6 +4,31 @@ import UIKit
 import Foundation
 
 /*
+Reverse words in an Array 
+*/
+
+var message = [ "c", "a", "k", "e", " ","p", "o", "u", "n", "d", " ","s", "t", "e", "a", "l" ]
+var reverseWords = { (message : [String]) -> (String) in
+	var lastIndex = message.count-1
+	var prevIndex = message.count-1
+	var wordToBeReturned = String()
+	while lastIndex > 0 {
+		if message[lastIndex] == " " {
+			// extract the word from this index to the previous stored index value
+			let word = message[lastIndex...prevIndex]
+			wordToBeReturned += word.joined()
+			prevIndex = lastIndex
+		}
+		lastIndex -= 1
+	}
+	let word = message[lastIndex...prevIndex]
+	wordToBeReturned += word.joined()
+	return wordToBeReturned
+}
+
+print(reverseWords(message))
+
+/*
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 Example:
